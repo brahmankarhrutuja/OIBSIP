e535f793749670dc66e82badc32da0f6d03e01f2
Hrutuja Rahul Brahmankar <141763509+brahmankarhrutuja@users.noreply.github.com>
2023-09-27 23:44:20 +0530

    Update script.js

--- Task 3/script.js
+++ Task 3/script.js
@@ -2,3 +2,4 @@ document.addEventListener('DOMContentLoaded', function () {
     const celsiusInput = document.getElementById('celsiusInput');
     const convertButton = document.getElementById('convertButton');
     const resultText = document.getElementById('resultText');
+    const resultValue = document.getElementById('resultValue');
