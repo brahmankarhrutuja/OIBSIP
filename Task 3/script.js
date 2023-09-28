c3336aa1cd905b3dcb858eb0d62a10308bd24361
Hrutuja Rahul Brahmankar <141763509+brahmankarhrutuja@users.noreply.github.com>
2023-09-28 23:55:41 +0530

    Update script.js

--- Task 3/script.js
+++ Task 3/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const celsiusInput = document.getElementById('celsiusInput');
     const convertButton = document.getElementById('convertButton');
     const resultText = document.getElementById('resultText');
-    const resultValue = document.getElementById('resultValue');
+    const resultValue = document.getElementById('resultValue'); 
      convertButton.addEventListener('click', function () {
         const celsius = parseFloat(celsiusInput.value);
          if (!isNaN(celsius)) {
